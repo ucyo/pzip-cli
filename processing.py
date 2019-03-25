2b6fab4c16daa801170387aea0c7a0eeb29bd9ee
ucyo <cayoglu@me.com>
2019-03-25 16:54:28 +0100

    Usage info

--- processing.py
+++ processing.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""Plotting a stream of data"""
+"""
+Plotting a stream of data
+Usage: python script.py filename
+"""
 
 from glob import glob
 import matplotlib.pyplot as plt
