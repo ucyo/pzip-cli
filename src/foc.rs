b6df0436e3bbf255e75ca7d21d869913b568de03
ucyo <cayoglu@me.com>
2019-04-25 21:20:20 +0200

    [feature] add function environment

--- src/foc.rs
+++ src/foc.rs
@@ -5,5 +5,28 @@ use super::graycodeanalysis::read_u32;
 pub fn foc(matches: &clap::ArgMatches) {
     // println!("{:#?}", matches);
     let ifile = String::from(matches.value_of("input").unwrap());
+    let mode = String::from(matches.value_of("mode").unwrap());
     let data = read_u32(&ifile);
+
+    if mode == "diff" {
+        process_diff(&data);
+    } else if mode == "xor" {
+        process_xor(&data);
+    } else if mode == "power" {
+        process_power(&data);
+    } else {
+        println!("Can not understand!");
+    }
+}
+
+fn process_diff(data: &Vec<u32>) {
+    unimplemented!()
+}
+
+fn process_xor(data: &Vec<u32>) {
+    unimplemented!()
+}
+
+fn process_power(data: &Vec<u32>) {
+    unimplemented!()
 }
