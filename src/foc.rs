022b4292df3b5516c1ba0b6a9aee602324589ace
ucyo <cayoglu@me.com>
2019-04-25 22:54:00 +0200

    [feature] add log messages

--- src/foc.rs
+++ src/foc.rs
@@ -1,5 +1,6 @@
 //! Reading file and performing a first ones count compression
 //!
+use log::{debug, info};
 use std::fs::metadata;
 use super::graycodeanalysis::read_u32;
 use bit_vec::BitVec;
@@ -11,6 +12,7 @@ pub fn foc(matches: &clap::ArgMatches) {
     let ifile = String::from(matches.value_of("input").unwrap());
     let mode = String::from(matches.value_of("mode").unwrap());
     let data = read_u32(&ifile);
+    info!("Encode file: {} with mode: {}", ifile, mode);
 
     let mut outbytes = 0usize;
     if mode == "diff" {
@@ -31,17 +33,21 @@ pub fn foc(matches: &clap::ArgMatches) {
 // Implementation of
 // huff(lzc) + raw(sign) + huff(6-residual) + raw(residual)
 fn process_diff(data: &Vec<u32>, n: u32) -> usize {
+    info!("Cut: {}", n);
     let mut signs = BitVec::new();
     for _ in 0..data.len() {
         signs.push(true);
     }
     let signs = signs.to_bytes();  // raw(sign)
+    debug!("Size: Raw(Sign)={}", signs.len());
 
     let lzc = data.iter().map(|&x| x.leading_zeros() as u8).collect::<Vec<u8>>();
     let lzc = encode(&lzc);  // huff(lzc)
+    debug!("Size: Huff(LZC)={}", lzc.len());
 
     let first6 = data.iter().map(|&x| get_value_first(&x, n) as u8).collect::<Vec<u8>>();
     let first6 = encode(&first6);  // huff(6-residual)
+    debug!("Size: Huff(6-residual)={}", first6.len());
 
     let leftresidual = data.iter().map(|&x| get_residual_size_after(&x, n)).sum::<u32>();
     let mut _bv = BitVec::new();
@@ -49,6 +55,7 @@ fn process_diff(data: &Vec<u32>, n: u32) -> usize {
         _bv.push(true);
     }
     let leftresidual = _bv.to_bytes();  // raw(residual - 6)
+    debug!("Size: Raw(residual-6)={}", leftresidual.len());
 
     return signs.len() + lzc.len() + first6.len() + leftresidual.len()
 }
