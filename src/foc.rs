fc4191cd11c336baad91f5befd3879d1f556a6d6
ucyo <cayoglu@me.com>
2019-04-25 17:28:59 +0200

    [feature] read input file

--- src/foc.rs
+++ src/foc.rs
@@ -1,3 +1,9 @@
+//! Reading file and performing a first ones count compression
+//!
+use super::graycodeanalysis::read_u32;
+
 pub fn foc(matches: &clap::ArgMatches) {
-    println!("{:#?}", matches);
+    // println!("{:#?}", matches);
+    let ifile = String::from(matches.value_of("input").unwrap());
+    let data = read_u32(&ifile);
 }
